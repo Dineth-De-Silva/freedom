7b72dea831e641512cea00e56c4c52f909bca268
D. A. Dineth De Silva <dadinethdesilva@gmail.com>
2021-03-23 22:10:45 +0530

    Initial commit (Flutter, Firebase, Theme)

---
+++ android/app/src/main/kotlin/com/example/freedom/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.freedom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:freedom/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Freedom());
+}
+
+class Freedom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Freedom',
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        iconTheme: IconThemeData(color: kContentColorLightTheme),
+        textTheme: GoogleFonts.latoTextTheme(Theme.of(context).textTheme),
+        colorScheme: ColorScheme.light(
+          primary: kPrimaryColor,
+          secondary: kSecondaryColor,
+          error: kErrorColor,
+        ),
+      ),
+      home: Scaffold(),
+    );
+  }
+}
